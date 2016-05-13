cd53105a646c137a5a655a9eb7e7992b0bdd1111
Rosando Desouza <rosando@CreativeCapsule.local>
2016-05-14 02:29:48 +0530

    to do app

---
+++ Scripts/general.js
@@ -0,0 +1,31 @@
+﻿var main = function () {
+
+    $('form').submit(function (event) {
+        event.preventDefault();
+
+        var $input = $(event.target).find('input');
+        var comment = $input.val();
+
+        if (comment.trim() != "") {
+            var html = $('<li>', {
+                            text: comment
+                        });
+            var remove = $('<span>', {
+                            'class': 'remove glyphicon glyphicon-remove'
+                        });
+            remove.appendTo(html);
+            html.prependTo('#comments');
+        }
+
+        $input.val('');
+
+        return false;
+    });
+
+    $('body').on('click', '.remove', function () {
+        $(this).parent().remove();
+    });
+
+}
+
+$(document).ready(main);
